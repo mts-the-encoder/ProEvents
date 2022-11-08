763c03dc080438ed533e8f6090cb796295740eb2
mts-the-encoder <matheus.nsantos@bandtec.com.br>
2022-11-08 09:46:53 -0300

    add new components

--- Front/ProEvents-App/src/app/app.module.ts
+++ Front/ProEvents-App/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EventsComponent } from './events/events.component';
+import { SpeakersComponent } from './speakers/speakers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventsComponent,
+    SpeakersComponent
   ],
   imports: [
     BrowserModule,
