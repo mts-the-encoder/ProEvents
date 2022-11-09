e07306dc5b63ab516427bb57eb41e69a35335d42
mts-the-encoder <matheus.nsantos@bandtec.com.br>
2022-11-09 14:10:07 -0300

    ngif

--- Front/ProEvents-App/src/app/events/events.component.ts
+++ Front/ProEvents-App/src/app/events/events.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventsComponent implements OnInit {
 
-  public events: any;
+  public events: any = [];
 
   constructor(private http: HttpClient) { }
 
