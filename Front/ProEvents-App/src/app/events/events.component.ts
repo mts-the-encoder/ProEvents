8e009d727b0a2e603f9adc46840cda28b42ae975
mts-the-encoder <matheus.nsantos@bandtec.com.br>
2022-11-10 12:36:48 -0300

    grid filter

--- Front/ProEvents-App/src/app/events/events.component.ts
+++ Front/ProEvents-App/src/app/events/events.component.ts
@@ -9,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 export class EventsComponent implements OnInit {
 
   public events: any = [];
+  public filteredEvents: any = [];
   widthImg: number = 50;
   marginImg: number = 2;
   showImg: boolean = true;
+  private _listFilter : string = '';
+
+  public get listFilter() {
+    return this._listFilter;
+  }
+
+  public set listFilter(value: string) {
+    this._listFilter = value;
+    this.filteredEvents = this.listFilter ? this.filterEvent(this.listFilter) : this.events;
+  }
+
+  filterEvent(filterBy: string): any {
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.events.filter((event: { theme: string; place: string; }) => event.theme.toLocaleLowerCase().indexOf(filterBy) !== -1 ||
+    event.place.toLocaleLowerCase().indexOf(filterBy) !== -1);
+  }
 
   constructor(private http: HttpClient) { }
 
@@ -25,8 +42,11 @@ export class EventsComponent implements OnInit {
 
   public getEvents(): void {
     this.http.get('https://localhost:7242/api/events').subscribe(
-      response => this.events = response,
+      response => {
+        this.events = response;
+        this.filteredEvents = this.events
+      },
       error => console.log(error)
-    );
+      );
   }
 }
