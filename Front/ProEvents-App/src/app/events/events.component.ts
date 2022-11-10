d05875ff04375d19b6039e91218ca71ce6f9aaa8
mts-the-encoder <matheus.nsantos@bandtec.com.br>
2022-11-10 08:55:43 -0300

    data biding img

--- Front/ProEvents-App/src/app/events/events.component.ts
+++ Front/ProEvents-App/src/app/events/events.component.ts
@@ -11,20 +11,22 @@ export class EventsComponent implements OnInit {
   public events: any = [];
   widthImg: number = 50;
   marginImg: number = 2;
+  showImg: boolean = true;
 
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.getEvents();
+  }
 
+  changeImg() {
+    this.showImg = !this.showImg;
+  }
+
+  public getEvents(): void {
     this.http.get('https://localhost:7242/api/events').subscribe(
       response => this.events = response,
       error => console.log(error)
     );
-
-    this.getEvents();
-  }
-
-  public getEvents(): any {
-
   }
 }
