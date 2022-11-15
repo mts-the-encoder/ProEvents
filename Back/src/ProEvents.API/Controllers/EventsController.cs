589195b8e0bd993be6b812055fa83da5718fe689
mts-the-encoder <matheus.nsantos@bandtec.com.br>
2022-11-15 01:40:25 -0300

    gets controller

--- Back/src/ProEvents.API/Controllers/EventsController.cs
+++ Back/src/ProEvents.API/Controllers/EventsController.cs
@@ -1,6 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
-using ProEvents.Domain;
-using ProEvents.Persistence.Context;
+using ProEvents.Application.Contracts;
 
 namespace ProEvents.API.Controllers;
 
@@ -8,15 +7,64 @@ namespace ProEvents.API.Controllers;
 [Route("api/[controller]")]
 public class EventsController : ControllerBase
 {
-    private readonly ProEventsContext _context;
-    public EventsController(ProEventsContext context)
+    private readonly IEventService _service;
+
+    public EventsController(IEventService service)
     {
-        _context = context;
+        _service = service;
     }
 
     [HttpGet]
-    public IEnumerable<Event> Get()
+    public async Task<IActionResult> Get()
     {
-        return _context.Events;
+        try
+        {
+            var events = await _service.GetAllEventsAsync(true);
+
+            if (events == null) return NotFound("No events found");
+
+            return Ok(events);
+        }
+        catch (Exception e)
+        {
+            return this.StatusCode(StatusCodes.Status500InternalServerError,
+                $"Error trying to retrieve events. Error: {e.Message}");
+        }
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetById(int id)
+    {
+        try
+        {
+            var res = await _service.GetEventsByIdAsync(id, true);
+
+            if (res == null) return NotFound("No event by Id found");
+
+            return Ok(res);
+        }
+        catch (Exception e)
+        {
+            return this.StatusCode(StatusCodes.Status500InternalServerError,
+                $"Error trying to retrieve event. Error: {e.Message}");
+        }
+    }
+
+    [HttpGet("theme/{theme}")]
+    public async Task<IActionResult> GetByTheme(string theme)
+    {
+        try
+        {
+            var res = await _service.GetAllEventsByThemeAsync(theme, true);
+
+            if (res == null) return NotFound("No events by theme are found");
+
+            return Ok(res);
+        }
+        catch (Exception e)
+        {
+            return this.StatusCode(StatusCodes.Status500InternalServerError,
+                $"Error trying to retrieve events. Error: {e.Message}");
+        }
     }
 }
