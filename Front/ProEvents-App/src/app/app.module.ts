5f3b7bae329c0ebc4b5ee1b04564322ce43db62f
mts-the-encoder <matheus.nsantos@bandtec.com.br>
2022-11-21 03:31:26 -0300

    DAteFormat

--- Front/ProEvents-App/src/app/app.module.ts
+++ Front/ProEvents-App/src/app/app.module.ts
@@ -1,9 +1,12 @@
-import { EventService } from './services/event.service';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
+
+import { CollapseModule } from 'ngx-bootstrap/collapse';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,14 +14,16 @@ import { SpeakersComponent } from './speakers/speakers.component';
 import { EventsComponent } from './events/events.component';
 import { NavComponent } from './nav/nav.component';
 
-import { CollapseModule } from 'ngx-bootstrap/collapse';
+import { EventService } from './services/event.service';
 
+import { DateTimeFormatPipe } from './helpers/DateTimeFormat.pipe';
 @NgModule({
   declarations: [
     AppComponent,
     SpeakersComponent,
     EventsComponent,
-    NavComponent
+    NavComponent,
+    DateTimeFormatPipe
   ],
   imports: [
     BrowserModule,
@@ -26,7 +31,9 @@ import { CollapseModule } from 'ngx-bootstrap/collapse';
     HttpClientModule,
     BrowserAnimationsModule,
     CollapseModule.forRoot(),
-    FormsModule
+    FormsModule,
+    TooltipModule.forRoot(),
+    BsDropdownModule.forRoot()
   ],
   providers: [
     EventService
---
+++ Front/ProEvents-App/src/app/util/constants.ts
@@ -0,0 +1,4 @@
+export class Constants {
+  static readonly DATE_FMT = 'MM/dd/yyyy';
+  static readonly DATE_TIME_FMT = `${Constants.DATE_FMT} hh:mm`;
+}
