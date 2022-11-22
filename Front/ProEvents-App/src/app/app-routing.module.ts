6f64d9784e518db97189aa06134e307aad132ebe
mts-the-encoder <matheus.nsantos@bandtec.com.br>
2022-11-22 14:57:10 -0300

    routes

--- Front/ProEvents-App/src/app/app-routing.module.ts
+++ Front/ProEvents-App/src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
+import { EventsComponent } from './components/events/events.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { SpeakersComponent } from './components/speakers/speakers.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { ContactsComponent } from './components/contacts/contacts.component';
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'events', component: EventsComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'speakers', component: SpeakersComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'contacts', component: ContactsComponent },
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: '**', redirectTo: 'dashboard', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
