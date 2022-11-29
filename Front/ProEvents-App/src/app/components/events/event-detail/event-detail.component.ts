c28fd492737bf51c18499c7068fb24178dafbab8
mts-the-encoder <matheus.nsantos@bandtec.com.br>
2022-11-29 15:53:55 -0300

    reactive form 2

--- Front/ProEvents-App/src/app/components/events/event-detail/event-detail.component.ts
+++ Front/ProEvents-App/src/app/components/events/event-detail/event-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-event-detail',
@@ -17,13 +17,17 @@ export class EventDetailComponent implements OnInit {
 
   public validation(): void {
     this.form = new FormGroup({
-      local: new FormControl(),
-      eventDate: new FormControl(),
-      theme: new FormControl(),
-      qtdPeoples: new FormControl(),
-      imageURL: new FormControl(),
-      phone: new FormControl(),
-      email: new FormControl()
+      theme: new FormControl('',
+        [Validators.required, Validators.minLength(4), Validators.maxLength(50)]),
+      local: new FormControl('', Validators.required),
+      eventDate: new FormControl('',
+        [Validators.required, Validators.pattern("MM/dd/yyyy")]),
+      qtdPeoples: new FormControl('',
+        [Validators.required, Validators.max(120000)]),
+      phone: new FormControl('', Validators.required),
+      email: new FormControl('',
+        [Validators.required, Validators.email]),
+      imageURL: new FormControl('', Validators.required)
     });
   }
 
