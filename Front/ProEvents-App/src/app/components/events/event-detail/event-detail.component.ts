a7802870493540b4465859c7fc936373aaa5f71e
mts-the-encoder <matheus.nsantos@bandtec.com.br>
2022-11-30 02:32:47 -0300

    formBulder

--- Front/ProEvents-App/src/app/components/events/event-detail/event-detail.component.ts
+++ Front/ProEvents-App/src/app/components/events/event-detail/event-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-event-detail',
@@ -10,25 +10,25 @@ export class EventDetailComponent implements OnInit {
 
   form!: FormGroup;
 
-  constructor() { }
+  get f(): any {
+    return this.form.controls;
+  }
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
     this.validation();
   }
 
   public validation(): void {
-    this.form = new FormGroup({
-      theme: new FormControl('',
-        [Validators.required, Validators.minLength(4), Validators.maxLength(50), Validators.nullValidator]),
-      local: new FormControl('', Validators.required),
-      eventDate: new FormControl('',
-        [Validators.required, Validators.pattern("MM/dd/yyyy")]),
-      qtdPeoples: new FormControl('',
-        [Validators.required, Validators.max(120000)]),
-      phone: new FormControl('', Validators.required),
-      email: new FormControl('',
-        [Validators.required, Validators.email]),
-      imageURL: new FormControl('', Validators.required)
+    this.form = this.fb.group({
+      theme: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(50), Validators.nullValidator]],
+      local: ['', Validators.required],
+      eventDate: ['', [Validators.required, Validators.pattern("MM/dd/yyyy")]],
+      qtdPeoples: ['', [Validators.required, Validators.max(120000)]],
+      phone: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      imageURL: ['', Validators.required]
     });
   }
 }
