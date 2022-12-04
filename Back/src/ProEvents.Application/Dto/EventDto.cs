d7f348f46680dff5aa0b85a2c4c0ed5ca010afcf
mts-the-encoder <matheus.nsantos@bandtec.com.br>
2022-12-04 04:29:13 -0300

    email, qtd and phone validation

--- Back/src/ProEvents.Application/Dto/EventDto.cs
+++ Back/src/ProEvents.Application/Dto/EventDto.cs
@@ -13,13 +13,18 @@ namespace ProEvents.Application.Dto
          MaxLength(50, ErrorMessage = "Is longer than the 50 characters")]
         public string Theme { get; set; }
 
-
+        [Range(100,120000, ErrorMessage = "must be at range 100 and 120.000")]
         public int QtdPeople { get; set; }
         public string ImageURL { get; set; }
+
+        [Required(ErrorMessage = "{0} is required")]
+        [Phone(ErrorMessage = ("Please enter a valid phone number"))]
         public string Phone { get; set; }
 
+        [Required, Display(Name = "E-mail")]  //Only for learn
         [EmailAddress(ErrorMessage = "Field {0} is not a valid email")]
-        [Required]
+        [RegularExpression(@"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$",
+            ErrorMessage = "Please enter a valid e-mail address")]  //Only 4fun
         public string Email { get; set; }
 
         public IEnumerable<LotDto> Lots { get; set; }
