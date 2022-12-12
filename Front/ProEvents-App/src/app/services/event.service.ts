85c6f30bd3418327fcc865f01dd42e9f1c674b87
mts-the-encoder <matheus.nsantos@bandtec.com.br>
2022-12-12 02:05:36 -0300

    post, put and delete

--- Front/ProEvents-App/src/app/services/event.service.ts
+++ Front/ProEvents-App/src/app/services/event.service.ts
@@ -1,4 +1,4 @@
-import { Event } from '../models/Event';
+import { Event } from './../models/Event';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
@@ -18,5 +18,17 @@ export class EventService {
 
   public getEventById(id: number): Observable<Event> {
     return this.http.get<Event>(`${this.baseURL}/${id}`);
+  }
+
+  public postEvent(event: Event): Observable<Event> {
+    return this.http.post<Event>(this.baseURL, event);
+  }
+
+  public putEvent(id: number, event: Event): Observable<Event> {
+    return this.http.put<Event>(`${this.baseURL}/${id}`, event);
+  }
+
+  public deleteEvent(id: number): Observable<string> {
+    return this.http.delete<string>(`${this.baseURL}/${id}`);
   }
 }
