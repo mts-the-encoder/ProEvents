1e200827e82c2edf8639de5e7f3c2fc363e4b5fc
mts-the-encoder <matheus.nsantos@bandtec.com.br>
2022-12-14 10:59:08 -0300

    lots endpoints

---
+++ Back/src/ProEvents.Persistence/Contracts/ILotPersist.cs
@@ -0,0 +1,10 @@
+﻿using ProEvents.Domain;
+
+namespace ProEvents.Persistence.contracts
+{
+    public interface ILotPersist
+    {
+        Task<Lot?[]> GetLotsByEventId(int eventId);
+        Task<Lot?> GetLotByIdAsync(int eventId, int id);
+    }
+}
